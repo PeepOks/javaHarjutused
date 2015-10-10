84b6faef5e46060c12807315da772a0ca2886d98
PeepOKS <peep.oksa@gmail.com>
2015-10-10 20:55:26 +0300

    Harjutus2 lõpuni

--- src/teema1/Harjutus2_meetodid.java
+++ src/teema1/Harjutus2_meetodid.java
@@ -1,5 +1,7 @@
 package teema1;
 
+import java.util.Scanner;
+
 /**
  * 1. Kirjutada meetod, mis tagastab parameetrina etteantud arvu kuubi.
  * 2. Kirjutada meetod, mis saab parameetritena kaks arvu (vahemiku) ning
@@ -10,13 +12,34 @@ package teema1;
 public class Harjutus2_meetodid {
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("1. Ülesanne");
+        System.out.println("Sisesta täisarv: ");
+        int arv = sc.nextInt();;
+        System.out.println("Arvu " + arv + " kuup on " + tostaKuupi(arv));
+
+        System.out.println("2. Ülesanne");
+        System.out.println("sobiv arv on: " + kasutajaSisestus("Sisesta arv vahemikus 5 kuni 10", 5, 10));
+
     }
 
     public static int tostaKuupi(int arv) {
-        return 0;
+        int arvKuubis = arv*arv*arv;
+        return arvKuubis;
     }
 
     public static int kasutajaSisestus(String kysimus, int min, int max) {
-        return 0;
+
+        System.out.println(kysimus + ": ");
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Arv: ");
+        int arv = sc.nextInt();
+
+        while (arv < min | arv > max) {
+            System.out.println("Arv " + arv + " ei sobi. Sisesta õige number.");
+            System.out.print("Arv: ");
+            arv = sc.nextInt();
+        }
+        return arv;
     }
 }
